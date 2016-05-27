36e3a2c6a5c9fc5a4bbd4c504510e927a0c69695
Noah Yang <Noah Yang>
2016-05-27 21:41:51 +0900

    Add webpack & babel transpiler
    Refactor with babel and webpack

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import router from './router';
+
+const app = express();
+
+let port = 8000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// 경로 '/' 로 들어오는 요청들은 public 폴더로 정적 라우팅합니다.
+app.use(express.static(__dirname + '/../public'));
+app.use('/', router);
+
+app.get('/hello', (req, res) => {
+    return res.send('Can you hear me?');
+});
+
+const server = app.listen(port, () => {
+  console.log('Express listening on port', port);
+});
