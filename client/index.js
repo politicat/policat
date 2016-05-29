90b83f010448dcc95d4e620e89f778e665595eb6
Noah Yang <Noah Yang>
2016-05-29 18:39:25 +0900

    (refactor) Modify client/index.js
    Delete search component
    Add main button click event(d3 initialization)

--- client/index.js
+++ client/index.js
@@ -23,7 +23,6 @@ class Main extends React.Component {
       url: '/search',
       data: {data : this.refs.searchInput.value}
     }).done(function(data) {
-      //console.log('value? ', data);
       mainD3(data, root);
     });
     this.refs.searchInput.value = '';
@@ -37,22 +36,10 @@ class Main extends React.Component {
     this.postSearch();
   }
   render() {
+    d3.select('svg').remove();
     return (
       <div>
-        <h2> Main </h2>
         <input type="text" placeholder="Input" ref="searchInput" onKeyPress={this.searchEnterkey}/>
-        <button onClick={this.searchClick}>Search</button>
-      </div>
-    );
-  }
-}
-
-class Search extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2> Search </h2>
-        <input type="text" placeholder="Input" ref="searchInput"/>
         <button onClick={this.searchClick}>Search</button>
       </div>
     );
@@ -63,10 +50,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to="">Main</Link></li>
-          <li><Link to="search">Search</Link></li>
-        </ul>
+        <Link to="/"><img src="./testimg.png" height="42" width="42"/>Main</Link>
         {this.props.children}
       </div>
     );
@@ -78,7 +62,5 @@ let rootElement = document.getElementById('app');
 ReactDOM.render(<Router history = {browserHistory}>
       <Route path = "/" component = {App}>
          <IndexRoute component = {Main} />
-         <Route path = "" component = {Main} />
-         <Route path = "search" component = {Search} />
       </Route>
    </Router>, rootElement);
