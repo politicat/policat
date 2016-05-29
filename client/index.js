b6c0ce37405b6cce55ceef8a64737227645e886f
Noah Yang <Noah Yang>
2016-05-29 17:12:37 +0900

    (feat)change css contents
    change color in circle(d3) and font

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 import $ from 'jquery';
-
 import d3 from 'd3';
 import galaxy from './D3/galaxy.js';
 import mainD3 from './D3/main-d3.js';
@@ -23,7 +22,7 @@ class Main extends React.Component {
       url: '/search',
       data: {data : this.refs.searchInput.value}
     }).done(function(data) {
-      //console.log('value? ', data);
+      console.log('enter!');
       mainD3(data, root);
     });
     this.refs.searchInput.value = '';
@@ -37,22 +36,10 @@ class Main extends React.Component {
     this.postSearch();
   }
   render() {
+    console.log('Main render');
     return (
       <div>
-        <h2> Main </h2>
         <input type="text" placeholder="Input" ref="searchInput" onKeyPress={this.searchEnterkey}/>
-        <button onClick={this.searchClick}>Search</button>
-      </div>
-    );
-  }
-}
-
-class Search extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2> Search </h2>
-        <input type="text" placeholder="Input" ref="searchInput"/>
         <button onClick={this.searchClick}>Search</button>
       </div>
     );
@@ -61,12 +48,10 @@ class Search extends React.Component {
 
 class App extends React.Component {
   render() {
+    console.log('App render');
     return (
       <div>
-        <ul>
-          <li><Link to="">Main</Link></li>
-          <li><Link to="search">Search</Link></li>
-        </ul>
+        <Link to="/"><img src="./testimg.png" height="42" width="42"/>Main</Link>
         {this.props.children}
       </div>
     );
@@ -77,8 +62,7 @@ let rootElement = document.getElementById('app');
 
 ReactDOM.render(<Router history = {browserHistory}>
       <Route path = "/" component = {App}>
-         <IndexRoute component = {Main} />
-         <Route path = "" component = {Main} />
-         <Route path = "search" component = {Search} />
+       <IndexRoute component = {Main} />
+       <Route path = "/main" component = {Main} />
       </Route>
    </Router>, rootElement);
