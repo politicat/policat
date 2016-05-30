4064e8f68d263d1f27bf36701a4bfe58c2e905ff
Noah Yang <Noah Yang>
2016-05-30 15:36:18 +0900

    (refactor) Add hover event
    Implement mouse hover event on d3
    Add favicon load
    Modify css(d3 circle)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import favicon from 'serve-favicon';
 import router from './router';
 import mongoose from 'mongoose';
 
@@ -9,11 +10,10 @@ mongoose.connect('mongodb://localhost/politicat');
 const app = express();
 
 let port = 8000;
-
+app.use(favicon(__dirname + '/../public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// 경로 '/' 로 들어오는 요청들은 public 폴더로 정적 라우팅합니다.
 app.use(express.static(__dirname + '/../public'));
 app.use('/', router);
 
